820b85092cca5c19c89e9660391354ae890e6531
Ben Wachira <gishuks14@gmail.com>
2025-06-02 20:35:36 +0300

    Update app configuration and structure; add AndroidManifest, MainActivity, and MainApplication files, enhance app.json with splash and icon settings, and update styles and colors for improved UI.

--- android/app/src/main/java/com/sendit/app/MainActivity.kt
+++ android/app/src/main/java/com/sendit/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sendit
+package com.sendit.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/sendit/app/MainApplication.kt
+++ android/app/src/main/java/com/sendit/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.sendit
+package com.sendit.app
 
 import android.app.Application
 import android.content.res.Configuration
